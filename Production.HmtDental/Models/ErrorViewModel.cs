1688b2c2b11272c57eb9a314e9c57efd25afcfd2
Phí Long <longpv21.it@gmail.com>
2022-01-10 15:40:20 +0700

    longpv push code base UI

---
+++ Production.HmtDental/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Production.HmtDental.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
